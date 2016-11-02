9aa104ea6316d328bffecec4bdc9f280316bacf0
Justus Adam <dev@justus.science>
2016-11-02 16:34:33 +0100

    QR compiles

--- bot/QR.hs
+++ bot/QR.hs
@@ -17,16 +17,18 @@ module QR where
 
 import Marvin.Prelude
 import Network.Wreq
-import Control.Lens.Aeson
+import Data.Aeson.Lens
 import Data.Aeson
+import Control.Lens
 
 script :: IsAdapter a => ScriptInit a
 script = defineScript "qr" $ 
     respond (r [CaseInsensitive] "qr (.*)") $ do
         match <- getMatch
-        let url = encodeURIComponent $ msg `indexEx` 1
+        let url = encodeURIComponent $ match `indexEx` 1 :: Text
         r <- liftIO $ get $ "https://api.qrserver.com/v1/read-qr-code/?fileurl=" ++ unpack url
         case eitherDecode' (r^.responseBody) :: Either String Value of
             Left err -> errorM $ "Unreadable json " ++ pack err
-            Right data_ ->
-                send $ json ^?! nth 0 . key "symbol" . nth 0 . key "data"
+            Right json ->
+                send $ json ^?! nth 0 . key "symbol" . nth 0 . key "data" . _String
+  where encodeURIComponent = undefined
